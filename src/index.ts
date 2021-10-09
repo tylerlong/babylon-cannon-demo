ec5badbb7c4c4b9bdd48dcdf148c96159eed9129
Tyler Long <tyler4long@gmail.com>
2021-10-08 19:50:20 -0700

    Use keyboard to rotate

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,29 @@
-import * as THREE from 'three';
-import * as CANNON from 'cannon-es';
+import {
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  Mesh,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Vector3,
+  Quaternion,
+} from 'three';
+import {World, Body, NaiveBroadphase, Vec3, Box} from 'cannon-es';
 
-let world: CANNON.World;
-let body: CANNON.Body;
-let shape;
-const timeStep = 1 / 60;
-let camera: THREE.PerspectiveCamera;
-let scene: THREE.Scene;
-let renderer: THREE.WebGLRenderer;
-let geometry;
-let material;
-let mesh: THREE.Mesh;
+let world: World;
+let body: Body;
+let camera: PerspectiveCamera;
+let scene: Scene;
+let renderer: WebGLRenderer;
+let mesh: Mesh;
 
 initThree();
 initCannon();
 animate();
 
-function initCannon() {
-  world = new CANNON.World();
-  world.gravity.set(0, 0, 0);
-  world.broadphase = new CANNON.NaiveBroadphase();
-
-  shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
-  body = new CANNON.Body({
-    mass: 1,
-  });
-  body.addShape(shape);
-  body.angularVelocity.set(0, 10, 0);
-  body.angularDamping = 0.5;
-  world.addBody(body);
-}
-
 function initThree() {
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(
+  scene = new Scene();
+  camera = new PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     1,
@@ -42,17 +31,50 @@ function initThree() {
   );
   camera.position.z = 5;
   scene.add(camera);
-
-  geometry = new THREE.BoxGeometry(2, 2, 2);
-  material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-
-  mesh = new THREE.Mesh(geometry, material);
+  const geometry = new BoxGeometry(2, 2, 2);
+  const material = new MeshBasicMaterial({color: 0xff0000, wireframe: true});
+  mesh = new Mesh(geometry, material);
   scene.add(mesh);
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
+  renderer = new WebGLRenderer();
+  renderer.setSize(800, 600);
   document.body.appendChild(renderer.domElement);
+}
+
+function initCannon() {
+  world = new World();
+  world.gravity.set(0, 0, 0);
+  world.broadphase = new NaiveBroadphase();
+  const shape = new Box(new Vec3(1, 1, 1));
+  body = new Body({
+    mass: 1,
+  });
+  body.addShape(shape);
+  body.angularVelocity.set(0, 10, 0);
+  body.angularDamping = 0.5;
+  world.addBody(body);
+  document.addEventListener('keydown', event => {
+    switch (event.key) {
+      case 'ArrowLeft': {
+        body.angularVelocity.y -= 10;
+        break;
+      }
+      case 'ArrowRight': {
+        body.angularVelocity.y += 10;
+        break;
+      }
+      case 'ArrowUp': {
+        body.angularVelocity.x -= 10;
+        break;
+      }
+      case 'ArrowDown': {
+        body.angularVelocity.x += 10;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  });
 }
 
 function animate() {
@@ -62,12 +84,9 @@ function animate() {
 }
 
 function updatePhysics() {
-  // Step the physics world
-  world.step(timeStep);
-
-  // Copy coordinates from Cannon.js to Three.js
-  mesh.position.copy(body.position as unknown as THREE.Vector3);
-  mesh.quaternion.copy(body.quaternion as unknown as THREE.Quaternion);
+  world.step(1 / 60);
+  mesh.position.copy(body.position as unknown as Vector3);
+  mesh.quaternion.copy(body.quaternion as unknown as Quaternion);
 }
 
 function render() {
