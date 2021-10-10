41d479dbdf56cf13bed332fbc3c13d9a5f8f55d7
Tyler Long <tyler4long@gmail.com>
2021-10-10 16:42:21 -0700

    First working version of cannon.js

--- src/babylon-manager.ts
+++ src/babylon-manager.ts
@@ -1,4 +1,7 @@
 import * as BABYLON from 'babylonjs';
+import * as CANNON from 'cannon-es';
+
+(global as any).CANNON = CANNON;
 
 // Create canvas
 const canvas = document.createElement('canvas');
@@ -16,6 +19,12 @@ const engine = new BABYLON.Engine(canvas, true, {
 const createScene = function () {
   // Create a basic BJS Scene object
   const scene = new BABYLON.Scene(engine);
+
+  // Add physics engine
+  const gravityVector = new BABYLON.Vector3(0, -9.81, 0);
+  const physicsPlugin = new BABYLON.CannonJSPlugin();
+  scene.enablePhysics(gravityVector, physicsPlugin);
+
   // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
   const camera = new BABYLON.FreeCamera(
     'camera1',
@@ -26,12 +35,14 @@ const createScene = function () {
   camera.setTarget(BABYLON.Vector3.Zero());
   // Attach the camera to the canvas
   camera.attachControl(canvas, false);
+
   // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
   const light = new BABYLON.HemisphericLight(
     'light1',
     new BABYLON.Vector3(0, 1, 0),
     scene
   );
+
   // Create a built-in "sphere" shape using the SphereBuilder
   const sphere = BABYLON.MeshBuilder.CreateSphere(
     'sphere1',
@@ -39,13 +50,28 @@ const createScene = function () {
     scene
   );
   // Move the sphere upward 1/2 of its height
-  sphere.position.y = 1;
+  sphere.position.y = 2;
+
   // Create a built-in "ground" shape;
   const ground = BABYLON.MeshBuilder.CreateGround(
     'ground1',
     {width: 6, height: 6, subdivisions: 2, updatable: false},
     scene
   );
+
+  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(
+    sphere,
+    BABYLON.PhysicsImpostor.SphereImpostor,
+    {mass: 1, restitution: 0.9},
+    scene
+  );
+  ground.physicsImpostor = new BABYLON.PhysicsImpostor(
+    ground,
+    BABYLON.PhysicsImpostor.BoxImpostor,
+    {mass: 0, restitution: 0.9},
+    scene
+  );
+
   // Return the created scene
   return scene;
 };
