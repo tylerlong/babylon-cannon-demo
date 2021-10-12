308e35d02ee15810855de6994fa3cfdbc3544b35
Tyler Long <tyler4long@gmail.com>
2021-10-12 16:23:35 -0700

    Display Level

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,21 @@ import Game from './game';
 
 (global as unknown as {CANNON: typeof CANNON}).CANNON = CANNON;
 
+const button = document.createElement('button');
+button.style.top = '32px';
+button.style.right = '32px';
+button.style.width = '128px';
+button.style.height = '32px';
+button.style.position = 'absolute';
+button.style.color = 'black';
+document.body.appendChild(button);
+
 let mazeSize = 3;
 let game: Game;
 const newLevel = () => {
   game?.dispose();
   game = new Game(mazeSize);
+  button.textContent = `Level ${(mazeSize - 1) / 2}`;
   mazeSize += 2;
   game.once('exit', () => newLevel());
 };
